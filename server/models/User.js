48e68d50565b98c638df1a7e358daf095f8fc342
Abdulmalik <ayodejiamzat@gmail.com>
2024-07-09 22:26:49 +0100

    auto-commit

--- server/models/User.js
+++ server/models/User.js
@@ -2,6 +2,7 @@ import mongoose from 'mongoose'
 
 const userSchema = mongoose.Schema(
 	{
+		email: { type: String, require: true, unique: false },
 		email: { type: String, require: true, unique: false }
 	},
 	{
