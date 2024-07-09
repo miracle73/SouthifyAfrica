105ab35a1cc37fea82fa5a9f6fb889d85b65766a
Abdulmalik <ayodejiamzat@gmail.com>
2024-07-09 22:31:24 +0100

    auto-commit

--- server/models/User.js
+++ server/models/User.js
@@ -8,7 +8,8 @@ const userSchema = mongoose.Schema(
 		need: { type: String, require: true },
 		favFeature: { type: String, require: true },
 		awarenessMedium: { type: String, require: true },
-		phone: { type: String, require: true },
+		country: { type: String, require: true },
+		age: { type: String, require: true },
 	},
 	{
 		timestamps: true,
