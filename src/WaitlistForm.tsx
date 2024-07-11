ba7e345779e0bc4321120739e15877ed73e46c4f
Abdulmalik <ayodejiamzat@gmail.com>
2024-07-11 12:30:16 +0100

    auto-commit

--- src/WaitlistForm.tsx
+++ src/WaitlistForm.tsx
@@ -55,9 +55,8 @@ const WaitlistForm = () => {
             //     },
             // });
             const { data } = await axios.post(`https://southifyafrica-1.onrender.com/api/user/`, requestBody)
-            // if (data.status === 'ok') {
-            //     alert("ok")
-            // }
+            if (data.status === 'ok') {
+            }
 
             // Assuming you want to handle success similarly
             setData({
