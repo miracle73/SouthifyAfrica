fffec2a23a56fd04e96c3981e5a800ca7f14bdb7
Abdulmalik <ayodejiamzat@gmail.com>
2024-07-11 12:31:28 +0100

    auto-commit

--- src/WaitlistForm.tsx
+++ src/WaitlistForm.tsx
@@ -75,10 +75,9 @@ const WaitlistForm = () => {
 
 
         } catch (error: any) {
-            error.response.data.message ? 
             console.error('There was an error!', error);
             setLoading(false);
-             : alert("Something went wrong")
+            error.response.data.message ? alert(error.response.data.message) : alert("Something went wrong")
         }
     };
 
